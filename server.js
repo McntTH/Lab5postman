fa945d331fd6832a7b738713ea26a84cc03a8dc7
McntTH <kanitnanjam@gmail.com>
2024-03-03 18:20:05 +0700

    Lab5

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Kanitnan Thongsakul 6404101354')
 })
 
 const books = require('./db')
@@ -22,19 +22,21 @@ app.get('/books/:id', (req, res) => {
 
 app.post('/books', (req, res) => {
   books.push(req.body)
+  res.status(201).json(req.body)
 })
 
 app.put('/books/:id', (req, res) => {
   const updateIndex = books.findIndex(book => book.id === req.params.id)
-  res.json(Object.assign(books[updateIndex], req.body))
+  res.status(200). json(Object.assign(books[updateIndex], req.body))
+})
+
+app.delete('/books/:id', (req, res) => {
+  const deletedIndex = books.findIndex(book => book.id === req.params.id)
+  delete books[deletedIndex];
+  res.status(200).json(req.body)
 })
 
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
 
-app.delete('/books/:id', (req, res) => {
-  const deletedIndex = books.findIndex(book => book.id === req.params.id)
-  delete books[deleteIndex];
-  res.status(200).json(req.body)
-})
